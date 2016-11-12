80f40654f0b782b5c6bef87194591f58b02f9f7f
Josue Guillen <jguillen004@student.butte.edu>
2016-11-12 20:43:13 +0000

    lab 1 ascii art

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,26 @@
+/*
+Created by: Josue Guillen
+Date: 11/12/2016
+Assignment: Create an image completely using characters (AsCII)
+*/ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "    $    " << endl;
+    cout << "   $$$   " << endl;
+    cout << " $$$$$$$ " << endl;
+    cout << "$$$$$$$$$" << endl;
+    cout << " $$$$$$$ " << endl;
+    cout << "  $$$$$  " << endl;
+    cout << "   $$$   " << endl;
+    cout << "    $    " << endl;
+    cout << "    $    " << endl;
+    cout << "         " << endl;
+    cout << "Who says money doesn't grow on trees?" << endl;
+    return 0;
+}
+
+
